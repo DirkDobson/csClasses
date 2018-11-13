7b915e16c0a450043d14f4a643066c894e279e2a
Dirk Dobson <dirkvancouver@live.com>
2018-11-12 22:29:01 -0700

    inial commit

---
+++ csClasses/csClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public class Person
+            {
+                public string Name;
+
+                public void Introduce()
+                {
+                    Console.WriteLine("Hi, my name is " + Name);
+            }
+            }
+        }
+    }
+}
